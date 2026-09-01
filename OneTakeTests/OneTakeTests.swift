8526830c96a98f42bd6f09bd6a2551bf3abde8a0
Noman Dhoni <92979541+nomandhoni-cs@users.noreply.github.com>
2026-09-01 16:07:44 +0600

    Initial Commit

---
+++ OneTakeTests/OneTakeTests.swift
@@ -0,0 +1,19 @@
+//
+//  OneTakeTests.swift
+//  OneTakeTests
+//
+//  Created by Abdullah Al Noman on 1/9/26.
+//
+
+import Testing
+@testable import OneTake
+
+struct OneTakeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
